280792df20e88458fbbf24e9d284dc82647b1e1a
min lee <115785155+blissay@users.noreply.github.com>
2022-10-24 17:08:06 +0900

    Add files via upload
    if?

---
+++ Cal_basic.java
@@ -0,0 +1,14 @@
+package workstudy;
+
+public class Cal_basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		if(i==0) {
+			System.out.println("i변수의 값은 0입니다");
+		}
+		System.out.println("감사합니다. 프로그램을 종료합니다.");
+	}
+
+}
